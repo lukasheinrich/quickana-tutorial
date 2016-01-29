7da11d0e1d974107fe13c72488cfb19b38f3c2d1
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-01-29 17:27:18 -0500

    go

--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,13 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Travis"
     fi
     #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO results_website > /dev/null
+    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO results_website &> /dev/null
     #go into directory and copy data we're interested in to that directory
     cd results_website
-    rsync -rv --exclude=.git  ../results/* .
+    rsync -rv --exclude=.git  ../results/* results
     #add, commit and push files
     git add -f .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq origin $BRANCH > /dev/null
+    git push -fq origin $BRANCH &> /dev/null
     echo -e "Deploy completed\n"
 fi
