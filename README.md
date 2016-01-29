d201b127a1950ab415d1a50a2ffcc917bf3d7bda
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-01-29 17:12:00 -0500

    go

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # quickana-tutorial
 [![Build Status](https://travis-ci.org/lukasheinrich/quickana-tutorial.svg?branch=master)](https://travis-ci.org/lukasheinrich/quickana-tutorial)
 
-An Example QuickAna analysis, complete with a Dockerfile to run the script.
+An Example QuickAna analysis, complete with a Dockerfile to run the script. Dummy Chane
--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.name "Travis"
     fi
     #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO results_website > /dev/null
+    git clone --quiet --branch=$BRANCH https://github.com/$TARGET_REPO results_website > /dev/null
     #go into directory and copy data we're interested in to that directory
     cd results_website
     rsync -rv --exclude=.git  ../results/* .
